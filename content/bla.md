51473267842503287048169e1e10eea2e71dfdf3
neocortex <rafael@wattx.io>
2016-10-23 21:11:46 +0200

    umclean commit

---
+++ content/bla.md
@@ -0,0 +1,7 @@
+Title: Rafael Schultze-Kraft
+Slug: about
+Date: 2016-06-08 20:00
+Category: About
+
+
+bla bla bla
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'neocortex'
+SITENAME = 'Something something'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATH = './plugins'
+PLUGINS = ['ipynb.markup']
+
+
+DEFAULT_CATEGORY = 'misc'
+#DISPLAY_PAGES_ON_MENU = True
+#DISPLAY_CATEGORIES_ON_MENU = True
+OUTPUT_PATH = 'output/'
+THEME = 'pelican-blue'
+MENUITEMS = (('Blog', SITEURL), ('About', SITEURL))
+
+LOAD_CONTENT_CACHE = False
